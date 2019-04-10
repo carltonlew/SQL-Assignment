2ee540b9822c09a9c6edac170b01a9f366032302
Carlton Lewis <carltonlew@yahoo.com>
2019-04-10 18:39:07 -0500

    Update SQL_Assignment.sql

--- SQL_Assignment.sql
+++ SQL_Assignment.sql
@@ -56,7 +56,9 @@ where first_name='HARPO';
 
 
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
-
+select a1.address, s.first_name, s.last_name
+from address as a1 join staff s
+on (a1.address_id=s.address_id);
 
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
 
@@ -67,4 +69,65 @@ where first_name='HARPO';
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
 
 
--- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+-- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. 
+-- List the customers alphabetically by last name:
+
+
+-- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
+-- As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
+-- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+
+
+
+-- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
+select first_name, last_name
+from actor
+where actor_id in
+
+(
+select actor_id
+from film_actor
+where film_id in
+
+(
+select film_id
+from film
+where title = 'Alone Trip'
+)
+);
+
+-- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+
+
+
+-- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+
+
+
+-- 7e. Display the most frequently rented movies in descending order.
+
+
+
+-- 7f. Write a query to display how much business, in dollars, each store brought in.
+
+
+
+-- 7g. Write a query to display for each store its store ID, city, and country.
+
+
+-- 7h. List the top five genres in gross revenue in descending order. 
+-- (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+
+
+-- 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. 
+-- Use the solution from the problem above to create a view. 
+-- If you haven't solved 7h, you can substitute another query to create a view.
+
+
+
+-- 8b. How would you display the view that you created in 8a?
+
+
+
+-- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+
